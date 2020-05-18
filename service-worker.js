e20ac24aea41a5d9e86560cda2ad9f2da9c3567c
TuanNV <nguyentuan060196@gmail.com>
2020-05-19 00:19:44 +0700

    back

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b964324d2b13dc9fa5f91ff2dc7516f5.js"
+  "/precache-manifest.4d0085856702cf79c5634eca0ba2c56a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-argon-design-system"});
